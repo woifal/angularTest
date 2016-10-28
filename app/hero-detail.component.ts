8376db7612829452d5f48e51a94d8c812b2d46bd
Wolfgang Dietersdorfer <Wolfgang Dietersdorfer>
2016-10-28 23:19:48 +0200

    before mini-detail..

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,42 @@
+// Keep the Input import for now, we'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
+
+@Component({
+	moduleId: module.id
+	, templateUrl: 'hero-detail.component.html'
+	, selector: 'my-hero-detail',
+})
+
+
+export class HeroDetailComponent implements OnInit {
+	@Input()
+	hero: Hero;
+
+	constructor(
+		private heroService: HeroService,
+		private route: ActivatedRoute,
+		private location: Location
+	) {}
+
+	ngOnInit(): void {
+	  alert("init hero-detail-component..");
+	  this.route.params.forEach((params: Params) => {
+		let id = +params['id'];
+		alert("showing hero >" + id + "<");
+		this.heroService.getHero(id)
+		  .then(hero => this.hero = hero);
+	  });
+	}
+
+	goBack(): void {
+		this.location.back();
+	}
+}
+
+
+
