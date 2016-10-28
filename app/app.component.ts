419538eccee92d9b782a002d690e42c3f31332d3
Wolfgang Dietersdorfer <Wolfgang Dietersdorfer>
2016-10-28 21:22:09 +0200

    after part5(services)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `,
+    styleUrls: ['app/app.css'],
+    providers: [HeroService]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+    title = 'Tour of Heroes';
+    heroes: Hero[];
+    selectedHero: Hero;
+    constructor(private heroService: HeroService) { }
+    getHeroes(): void {
+        //  this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+        //  this.heroes = this.heroService.getHeroesPlain();
+        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+   	}
+   	ngOnInit(): void {
+        this.getHeroes();
+    }
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+}
