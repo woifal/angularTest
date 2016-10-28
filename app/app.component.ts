8e43255a5568bb441cd2ce5b836167b127b1603d
Wolfgang Dietersdorfer <Wolfgang Dietersdorfer>
2016-10-28 23:20:32 +0200

    before mini-detail..

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,37 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
+
+
 @Component({
     selector: 'my-app',
     template: `
     <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-      <li *ngFor="let hero of heroes"
-        [class.selected]="hero === selectedHero"
-        (click)="onSelect(hero)">
-        <span class="badge">{{hero.id}}</span> {{hero.name}}
-      </li>
-    </ul>
-    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-  `,
-    styleUrls: ['app/app.css'],
-    providers: [HeroService]
+
+	<nav>
+		<a routerLink="/dashboard">Dashboard</a>
+		<a routerLink="/heroes">Heroes</a>
+	</nav>
+	
+	<router-outlet></router-outlet>
+  `
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
     title = 'Tour of Heroes';
-    heroes: Hero[];
-    selectedHero: Hero;
-    constructor(private heroService: HeroService) { }
-    getHeroes(): void {
-        //  this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-        //  this.heroes = this.heroService.getHeroesPlain();
-        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
-   	}
-   	ngOnInit(): void {
-        this.getHeroes();
-    }
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HeroesComponent }     from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService }         from './hero.service';
 import { AppComponent }  from './app.component';
 
+import { RouterModule }   from '@angular/router';
+
+import { AppRoutingModule }     from './app-routing.module';
+
 @NgModule({
-  imports: [ 
-	BrowserModule,
-	FormsModule
-  ],
-  declarations: [ 
-	AppComponent 
-    ,HeroDetailComponent
-  ],
-  bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule
+        , FormsModule
+		, AppRoutingModule
+    ],
+    declarations: [
+        AppComponent
+        , HeroDetailComponent
+        , HeroesComponent
+		, DashboardComponent
+    ],
+    providers: [
+        HeroService    ],
+   	bootstrap: [AppComponent]
 })
 export class AppModule { }
